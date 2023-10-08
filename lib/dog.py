57b5eb5ac767435cc2e71b60244d80d8606abdd3
Mary <omota4fidelity@gmail.com>
2023-10-08 13:00:38 +0100

    updated

--- lib/dog.py
+++ lib/dog.py
@@ -12,23 +12,32 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    def __init__(self, name="Fido"):
+    def __init__(self, name="Fido", breed = ""):
         self._name = name
+        self.breed = breed
 
     def get_name(self):
         return self._name
 
     def set_name(self, value):
         
-        if (type(value, str) and 1 <= len(value) <= 25) :
-            self._name = value
-        else:
+        if value == "" or type(value) in (int, float) or 1 > len(value) or len(value) > 25:
             print("Name must be a string between 1 and 25 characters.")
+            
+        else:
+            self._name = value
 
     name = property(get_name, set_name)
 
+    def get_breed(self):
+        return self.breed
+    
+    def set_breed(self, breed):
+        if breed in APPROVED_BREEDS:
+            self.breed = breed
 
 
+    breed = property(get_breed, set_breed)
 
 
 #  python3 lib/dog.py
--- lib/person.py
+++ lib/person.py
@@ -16,7 +16,19 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    def __init__(self, name):
+    def __init__(self, name, job):
         self.name = name
+        self.job = job
         pass
+
+    def get_name(self):
+        return self.name
     pass
+
+    def set_name(self, name):
+        if isinstance(name, str):
+            if name == "" or 1 < len(name) > 25:
+                print("Name must be string between 1 and 25 characters.")
+            else:
+                print("Name must be string between 1 and 25 characters.")
+    name = property(get_name, set_name)
